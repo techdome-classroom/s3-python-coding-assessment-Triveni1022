2cbc0f9ba24cc57a9124a780209ebe9c470ef8fa
Vivek Kumar Dhoke <ravishankar20041973@gmail.com>
2024-09-27 15:12:11 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,25 @@
-class Solution(object):
+class Solution:
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        pass
-
-
-
-
-
-
-
+        if len(s) == 0:
+            return True
+        
+        stack = []
+        return self.valid(s, stack)
     
-
-
-
-  
-
+    def valid(self, s, stack):
+        for char in s:
+            if char in '([{':
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                top_char = stack.pop()
+                if (char == ')' and top_char != '(') or \
+                   (char == ']' and top_char != '[') or \
+                   (char == '}' and top_char != '{'):
+                    return False
+        return not stack
